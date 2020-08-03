c3b095a4c3170f4fd7654af02a8c322d3e670fc6
Flomastermod <flomastermod@gmail.com>
2020-08-02 20:44:59 -0500

    ok

--- store/index.js
+++ store/index.js
@@ -18,12 +18,24 @@ actions is where we will make an API call that gathers the posts,
 and then commits the mutation to update it
 */
 export const actions = {
-    //this will be asynchronous
-    async getPosts({
-        state,
-        commit
-    }) {
-        //the first thing we’ll do is check if there’s any length to the posts array in state, which means we already called the API, so don’t do it again.
-        if (state.posts.length) return
-    }
-}
+    async getPosts({ state, commit }) {
+      if (state.posts.length) return
+      try {
+        let posts = await fetch( `https://css-tricks.com/wp-json/wp/v2/posts?page=1&per_page=20&_embed=1`
+        ).then(res => res.json())
+        posts = posts
+          .filter(el => el.status === "publish")
+          .map(({ id, slug, title, excerpt, date, tags, content }) => ({
+            id
+            slug,
+            title,
+            excerpt,
+            date,
+            tags,
+            content
+          }))
+        commit("updatePosts", posts)
+      } catch (err) {
+        console.log(err)
+      }
+   }
